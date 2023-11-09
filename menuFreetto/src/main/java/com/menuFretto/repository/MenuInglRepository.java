a59ed53ac04e747555f66f72e84a1663645b958e
Simona Tedesco <101811948+SimonaTed@users.noreply.github.com>
2023-11-09 15:12:25 +0100

    Delete menuFreetto directory

--- menuFreetto/src/main/java/com/menuFretto/repository/MenuInglRepository.java
+++
@@ -1,9 +0,0 @@
-package com.menuFretto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.menuFretto.entity.MenuInglEntity;
-
-public interface MenuInglRepository extends JpaRepository<MenuInglEntity, String>{
-
-}
--- menuFreetto/src/main/java/com/menuFretto/repository/MenuItaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.menuFretto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.menuFretto.entity.MenuItaEntity;
-
-public interface MenuItaRepository extends JpaRepository<MenuItaEntity,String> {
-
-}
