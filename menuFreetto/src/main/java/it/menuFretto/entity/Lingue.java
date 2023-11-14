a2a3700b66d9da1e02dcc8205ffec0f5422b6a99
eliodinucci <126082250+eliodinucci@users.noreply.github.com>
2023-11-14 14:50:43 +0100

    Add files via upload

---
+++ menuFreetto/src/main/java/it/menuFretto/entity/Lingue.java
@@ -0,0 +1,31 @@
+package it.menuFretto.entity;
+
+public class Lingue {
+	
+	String nome;
+	String url;
+	
+	
+	
+	public Lingue(String nome, String url) {
+		super();
+		this.nome = nome;
+		this.url = url;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
+	
+
+}
---
+++ menuFreetto/src/main/java/it/menuFretto/repository/StrutturaRepository.java
@@ -0,0 +1,9 @@
+package it.menuFretto.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import it.menuFretto.entity.StrutturaEntity;
+
+public interface StrutturaRepository extends JpaRepository<StrutturaEntity, Integer> {
+
+}
