a6b8a6bf2c8f2c203343d105701713d519e77b4c
eliodinucci <126082250+eliodinucci@users.noreply.github.com>
2023-12-11 11:10:08 +0100

    Delete menuFreetto directory
    ciao

--- menuFreetto/src/main/java/it/menuFretto/repository/StrutturaRepository.java
+++
@@ -1,9 +0,0 @@
-package it.menuFretto.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import it.menuFretto.entity.StrutturaEntity;
-
-public interface StrutturaRepository extends JpaRepository<StrutturaEntity, Integer> {
-
-}
