9638976ac3ffa942543b77d680523d82941d4387
Simona Tedesco <101811948+SimonaTed@users.noreply.github.com>
2024-01-05 17:49:28 +0100

    Delete FrettoCompleto directory

--- FrettoCompleto/src/main/java/it/menu/repository/MenuRepository.java
+++
@@ -1,9 +0,0 @@
-package it.menu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import it.menu.entity.MenuEntity;
-
-public interface MenuRepository extends JpaRepository<MenuEntity, Integer> {
-
-}
